e2be0562d7168510649e3e5c88ce3118ce57f36a
SondreB <sondre@outlook.com>
2024-12-22 17:25:14 +0100

    Fix some rename issues

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'angor-hub' title`, () => {
+  it(`should have the 'angor-profile' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angor-hub');
+    expect(app.title).toEqual('angor-profile');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, angor-hub');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, angor-profile');
   });
 });
